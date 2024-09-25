0bfff31aae9284eb609bf2011e4e68654d4554c7
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-09-25 08:43:38 -0400

    add App Colors and Flutter Utils package

---
+++ lib/UI/Utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff004182);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color lightBlue = Color(0xffEDF1F5);
+const Color yellowColor = Color(0xffFDD835);
+const Color darkBrownColor = Color(0xff2F2929);
+const Color redColor = Color(0xffBC3018);
+const Color blueColor = Color(0xff0973DD);
+const Color greenColor = Color(0xff02B935);
+const Color pinkColor = Color(0xffFF645A);
+const Color purpleColor = Color(0xff6A6695);
+const Color darkYellowColor = Color(0xffF89209);
+const Color goldColor = Color(0xffFBE1C8);
