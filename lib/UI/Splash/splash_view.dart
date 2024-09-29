8c33e9b4fb9d0625218f7150f6465aa4f9896f64
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-09-29 10:42:59 -0400

    create splash view

---
+++ lib/UI/Splash/splash_view.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:e_commerce/UI/Auth/Login/login_view.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+  static const String splashId = 'SplashView';
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, LoginView.loginViewId);
+    });
+    return Scaffold(
+      body: Image.asset(
+        'assets/images/Splash Screen.png',
+        fit: BoxFit.fill,
+        width: double.infinity,
+        height: double.infinity,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerce/UI/Auth/Login/login_view.dart';
+import 'package:e_commerce/UI/Splash/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const App());
@@ -9,6 +12,18 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      builder: (context, child) {
+        return MaterialApp(
+          initialRoute: SplashView.splashId,
+          debugShowCheckedModeBanner: false,
+          routes: {
+            SplashView.splashId: (context) => const SplashView(),
+            LoginView.loginViewId: (context) => const LoginView(),
+          },
+        );
+      },
+    );
   }
 }
