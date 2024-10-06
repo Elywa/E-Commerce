56f23beb0165233a4908ff08b293ffcc8d18efe1
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-10-06 22:34:52 +0300

    create register func in Auth Repo Impl

--- lib/Data/Repositories/repositories/auth_repo_impl.dart
+++ lib/Data/Repositories/repositories/auth_repo_impl.dart
@@ -5,10 +5,19 @@ import 'package:e_commerce/Domain/Entities/auth_response_entity/failures_entity.
 import 'package:e_commerce/Domain/Repositories/data%20source/auth_remote_data_source.dart';
 import 'package:e_commerce/Domain/Repositories/repositories/auth_repo.dart';
 
-class AuthRepoImpl implements  AuthRepository{
+class AuthRepoImpl implements AuthRepository {
+  AuthRemoteDataSource dataSource;
+  AuthRepoImpl({required this.dataSource});
   @override
-  Future<Either<FailuresEntity, AuthResultEntity>> register(String name, String email, String password, String rePassword, String phone) {
-   
+  Future<Either<FailuresEntity, AuthResultEntity>> register(String name,
+      String email, String password, String rePassword, String phone) async {
+    var either =
+        await dataSource.register(name, email, password, rePassword, phone);
+
+    return either.fold((failure) {
+      return left(failure);
+    }, (response) {
+      return right(response);
+    });
   }
-  
 }
