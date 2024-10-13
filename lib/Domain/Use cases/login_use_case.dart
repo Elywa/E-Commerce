5d27b088c80b31af79a73393ee8b45b8ed32b5e8
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-10-13 20:24:21 +0300

    modify LoginUseCase

--- lib/Domain/Use cases/login_use_case.dart
+++ lib/Domain/Use cases/login_use_case.dart
@@ -1,3 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/Domain/Entities/auth_response_entity/auth_result_entity.dart';
+import 'package:e_commerce/Domain/Entities/auth_response_entity/failures_entity.dart';
+import 'package:e_commerce/Domain/Repositories/repositories/auth_repo.dart';
+
 class LoginUseCase {
-  
+  AuthRepository authrepo;
+
+  LoginUseCase({required this.authrepo});
+
+  Future<Either<FailuresEntity, AuthResultEntity>> invoke(
+      String email, String password) {
+    return authrepo.login(email, password);
+  }
 }
