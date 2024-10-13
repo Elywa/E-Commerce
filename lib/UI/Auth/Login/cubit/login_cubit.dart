971cacdb7ead3e7f543387ebc8302eb8c45e0175
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-10-13 21:05:27 +0300

    create Login Cubit part 1

---
+++ lib/UI/Auth/Login/cubit/login_cubit.dart
@@ -0,0 +1,10 @@
+
+
+
+
+import 'package:e_commerce/UI/Auth/Login/cubit/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitialState());
+}
--- lib/UI/Auth/Register/Cubits/Register Cubit/register_cubit_state.dart
+++ lib/UI/Auth/Register/Cubits/Register Cubit/register_cubit_state.dart
@@ -1,18 +1,17 @@
 part of 'register_cubit_cubit.dart';
 
-@immutable
-sealed class RegisterCubitState {}
+class RegisterCubitState {}
 
-final class RegisterCubitInitial extends RegisterCubitState {}
+ class RegisterCubitInitial extends RegisterCubitState {}
 
-final class RegisterCubitLoading extends RegisterCubitState {}
+ class RegisterCubitLoading extends RegisterCubitState {}
 
-final class RegisterCubSuccess extends RegisterCubitState {
+ class RegisterCubSuccess extends RegisterCubitState {
   AuthResultEntity response;
   RegisterCubSuccess({required this.response});
 }
 
-final class RegisterCubitFailure extends RegisterCubitState {
+ class RegisterCubitFailure extends RegisterCubitState {
   final String errMessage;
   RegisterCubitFailure({required this.errMessage});
 }
