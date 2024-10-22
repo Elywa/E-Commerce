e0fe2ad31154cdb28a6414f712eb97a8876773da
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-10-22 16:02:08 +0300

    refactor code

--- lib/UI/Home/widgets/Home_View_Body.dart
+++ lib/UI/Home/widgets/Home_View_Body.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/UI/Home/cubits/Home%20Cubit/home_view_model_cubit.dart';
+import 'package:e_commerce/UI/Home/functions.dart';
 import 'package:e_commerce/UI/Home/tabs/favourite%20tab/favourite_tab_view.dart';
 import 'package:e_commerce/UI/Home/tabs/home%20tab/home_tab_view.dart';
 import 'package:e_commerce/UI/Home/tabs/product%20details%20tab/products_tab_details.dart';
@@ -25,82 +26,10 @@ class _HomeViewBodyState extends State<HomeViewBody> {
         return Scaffold(
           backgroundColor: AppColors.whiteColor,
           body: viewModel.tabs[viewModel.selectedIndex],
-          bottomNavigationBar: buildCustomBottomNavigationBar(),
+          bottomNavigationBar:
+              buildCustomBottomNavigationBar(context, viewModel),
         );
       },
-    );
-  }
-
-  Widget buildIcon(String assetPath, bool isSelected) {
-    return Container(
-      width: 43.w,
-      height: 40.h,
-      decoration: isSelected
-          ? const BoxDecoration(
-              color: AppColors.whiteColor,
-              shape: BoxShape.circle,
-            )
-          : null,
-      child: ImageIcon(
-        AssetImage(assetPath),
-        size: 18.sp, // Reduced icon size to avoid overflow
-        color: isSelected ? AppColors.primaryColor : AppColors.whiteColor,
-      ),
-    );
-  }
-
-  SafeArea buildCustomBottomNavigationBar() {
-    return SafeArea(
-      child: ClipRRect(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.r),
-          topRight: Radius.circular(20.r),
-        ),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * .088,
-          child: BottomNavigationBar(
-            onTap: (value) {
-              viewModel.changeTab(value);
-            },
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            selectedItemColor: AppColors.whiteColor,
-            backgroundColor: AppColors.primaryColor,
-            currentIndex: viewModel.selectedIndex,
-            items: [
-              BottomNavigationBarItem(
-                label: 'Home',
-                icon: buildIcon(
-                  'assets/images/home_unselected.png',
-                  viewModel.selectedIndex == 0,
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: 'Details',
-                icon: buildIcon(
-                  'assets/images/product_unselected.png',
-                  viewModel.selectedIndex == 1,
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: 'Favourites',
-                icon: buildIcon(
-                  'assets/images/favourite_unselected.png',
-                  viewModel.selectedIndex == 2,
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: 'Profile',
-                icon: buildIcon(
-                  'assets/images/profile_unselected.png',
-                  viewModel.selectedIndex == 3,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
