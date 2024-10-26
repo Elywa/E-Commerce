1dcd3a7e57e054d30c067ae8fd5bee55ae4db65f
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-10-27 01:03:26 +0300

    create get all categories use case

---
+++ lib/Domain/Use cases/get_all_categories_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/Domain/Entities/auth_response_entity/failures_entity.dart';
+import 'package:e_commerce/Domain/Entities/category_response_entity/category_response_entity.dart';
+import 'package:e_commerce/Domain/Repositories/repositories/home_tab_repo.dart';
+
+class GetAllCategoriesUseCase {
+  HomeTabRepo homeTabRepo;
+  GetAllCategoriesUseCase({required this.homeTabRepo});
+
+  Either<FailuresEntity, CategoryResponseEntity> invoke() {
+    return homeTabRepo.getCategories();
+  }
+}
