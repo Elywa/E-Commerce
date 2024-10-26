648a3eb592e9c60c461344994f65e2f5414e71dd
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-10-27 01:00:07 +0300

    create home  tab repo and data source

---
+++ lib/Domain/Repositories/repositories/home_tab_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/Domain/Entities/auth_response_entity/failures_entity.dart';
+import 'package:e_commerce/Domain/Entities/category_response_entity/category_response_entity.dart';
+
+abstract class HomeTabRepo {
+  Either<FailuresEntity, CategoryResponseEntity> getCategories();
+}
