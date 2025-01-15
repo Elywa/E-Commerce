0d46700229519eb11f908cf1dcd563da5f7a9a56
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-01-15 19:30:34 +0200

    finish domain layer for remove favourite product

--- lib/Domain/Repositories/repositories/favourite_repo.dart
+++ lib/Domain/Repositories/repositories/favourite_repo.dart
@@ -1,8 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce/Domain/Entities/auth_response_entity/failures_entity.dart';
 import 'package:e_commerce/Domain/Entities/get_user_wishlist_products_response_entity/get_user_wish_list_products_response_entity/get_user_wish_list_products_response_entity.dart';
+import 'package:e_commerce/Domain/Entities/remove_favourite_product_response_entity/remove_favourite_product_response_entity.dart';
 
 abstract class FavouriteRepo {
   Future<Either<FailuresEntity, GetUserWishListProductsResponseEntity>>
       getWishListProducts();
+
+  Future<Either<FailuresEntity, RemoveFavouriteProductResponseEntity>>
+      removeFavouriteProduct();
 }
---
+++ lib/Domain/Use cases/remove_favourite_product_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/Domain/Entities/auth_response_entity/failures_entity.dart';
+import 'package:e_commerce/Domain/Entities/remove_favourite_product_response_entity/remove_favourite_product_response_entity.dart';
+import 'package:e_commerce/Domain/Repositories/repositories/favourite_repo.dart';
+
+class RemoveFavouriteProductUseCase {
+  FavouriteRepo favouriteRepo;
+  RemoveFavouriteProductUseCase({required this.favouriteRepo});
+
+  Future<Either<FailuresEntity, RemoveFavouriteProductResponseEntity>>
+      invoke() {
+    return favouriteRepo.removeFavouriteProduct();
+  }
+}
